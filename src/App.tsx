ea186afb347d6463b2673d937e9a986929d1ed09
Aurtekh <ermolovichd007@gmail.com>
2023-05-19 02:59:57 +0300

    search hosting deploy error

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './scss/app.scss';
 import React from 'react';
 // import MainLayout from './layouts/MainLayout';
-import { Route, Routes } from 'react-router-dom';
-import Friends from './pages/Friends';
-import { Home } from './pages/Home';
-import { Message } from './pages/Message';
-import { News } from './pages/News';
-import { Search } from './pages/Search/Search';
-import { EditProfile } from './pages/EditProfile';
-import { Auth } from './pages/Auth';
+// import { Route, Routes } from 'react-router-dom';
+// import Friends from './pages/Friends';
+// import { Home } from './pages/Home';
+// import { Message } from './pages/Message';
+// import { News } from './pages/News';
+// import { Search } from './pages/Search/Search';
+// import { EditProfile } from './pages/EditProfile';
+// import { Auth } from './pages/Auth';
 
 // import { fetchAuthMe } from './redux/slices/auth';
-import { NotFound } from './pages/NotFound';
+// import { NotFound } from './pages/NotFound';
 // import { useAppDispatch } from './redux/store';
 
 function App() {
@@ -23,8 +23,8 @@ function App() {
 
   return (
     <>
-      <Routes>
-        {/* <Route path="/" element={<MainLayout />}> */}
+      {/* <Routes>
+        <Route path="/" element={<MainLayout />}>
         <Route path="/:id" element={<Home />} />
         <Route path="/im" element={<Message />} />
         <Route path="/im/:id" element={<Message />} />
@@ -34,8 +34,9 @@ function App() {
         <Route path="/edit" element={<EditProfile />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="*" element={<NotFound />} />
-        {/* </Route> */}
-      </Routes>
+        </Route>
+      </Routes> */}
+      Страница
     </>
   );
 }
